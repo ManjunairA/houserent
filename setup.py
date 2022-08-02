14398161e0ddb850688d530340011000afc3e0a0
ManjunairA <manjunathanairmanju@gmail.com>
2022-08-02 09:44:01 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+
+setup(
+    name= "housing-predictor",
+    version="0.0.1",
+    author="Manjunair",
+    description="Prject description",
+    packages=["housing"]
+)
